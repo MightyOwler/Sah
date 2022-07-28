e0067840657b9821a93b861bb964fe87741953f1
MightyOwler <jasa.knap@gmail.com>
2022-07-28 10:16:51 +0200

    Pravilno deluje registracija

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,6 +1,5 @@
-from importlib.resources import path
 import bottle
-from bottle import template, static_file, route, response
+#from bottle import template, static_file, route, response
 import model
 
 SKRIVNOST = "blablalba" #to je kasneje treba dati v neko datoteko
@@ -11,7 +10,7 @@ vse_skupaj = model.VseSkupaj.iz_datoteke(STANJE)
 def index():
     return bottle.template("views/osnova.tpl")
 
-@route('/static/<ime_dat:path>')
+@bottle.route('/static/<ime_dat:path>')
 def server_static(ime_dat):
   pot = 'static'
   return bottle.static_file(ime_dat, root=pot)
@@ -30,23 +29,21 @@ def registracija_get():
 
 @bottle.post("/registracija/")
 def prijava_post():
+    global vse_skupaj
     uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
     geslo_v_cistopisu = bottle.request.forms.getunicode("zasifrirano_geslo") 
     uporabnik = vse_skupaj.poisci_uporabnika(uporabnisko_ime, geslo_v_cistopisu)
     if uporabnik:
         return bottle.template("registracija.tpl", napaka="Uporabnik že obstaja!")
     else:
-        # treba je dodati uporabnika v json
-        #
         nov_uporabnik = model.Uporabnik(uporabnisko_ime, geslo_v_cistopisu)
         slovar_z_novim_uporabnikom = dict()
         slovar_z_novim_uporabnikom["uporabniki"] = vse_skupaj.v_slovar()["uporabniki"] + [{'uporabnisko_ime': uporabnisko_ime, 'zasifrirano_geslo': geslo_v_cistopisu}]
-        #slovar_z_novim_uporabnikom.v_datoteko(STANJE)
         # na tem mestu sem spremenil metodo v_datoteko iz model.py 
         # (json.dump(self.v_slovar()) --> json.dump(self))
-        print([vse_skupaj.v_slovar()["uporabniki"],
-                                                    {'uporabnisko_ime': uporabnisko_ime, 'zasifrirano_geslo': geslo_v_cistopisu}])
         model.VseSkupaj.v_datoteko(slovar_z_novim_uporabnikom, STANJE)
+        # naslednja vrstica je zato, da pravilno deluje prijava
+        vse_skupaj = model.VseSkupaj.iz_datoteke(STANJE)
         bottle.response.set_cookie("uporabnisko_ime", uporabnisko_ime, path="/", secret=SKRIVNOST)
         bottle.redirect("/")
         
@@ -72,7 +69,7 @@ def prijava_post():
 @bottle.post("/odjava/")
 def odjava_post():
     #uporabnisko_ime = bottle.request.get_cookie("uporabnisko_ime")
-    response.delete_cookie("uporabnisko_ime", path = "/", secret=SKRIVNOST)
+    bottle.response.delete_cookie("uporabnisko_ime", path = "/", secret=SKRIVNOST)
     bottle.redirect("/")
 
 
