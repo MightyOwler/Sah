5f9f5d603080d243bd06d4e7fe5d0a529e0ab8cb
MightyOwler <jasa.knap@gmail.com>
2022-08-19 11:58:04 +0200

    globina --> 3

--- static/sah_proti_racunalniku_beli.js
+++ static/sah_proti_racunalniku_beli.js
@@ -11,7 +11,7 @@ var nextMove = null; // zato, ker je težko delati z globalnimi spremenljivkami
 const pieceScore = {"Q": 9, "R": 5, "B": 3, "N": 3, "P": 1, "q": -9, "r": -5, "b": -3, "n": -3, "p": -1};
 const CHECKMATE = 1000;
 const STALEMATE = 0;
-const GLOBINA = 2; // to dela v teoriji, v praksi pa za vse večje od 2 dela zelo počasi
+const GLOBINA = 3; // to dela v teoriji, v praksi pa za vse večje od 2 dela zelo počasi
 
 board.addEventListener('drag-start', (e) => {
   const { source, piece, position, orientation } = e.detail;
