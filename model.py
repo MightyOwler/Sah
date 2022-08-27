b4a06e09e9983277c1a3630244892ff02d288fdf
MightyOwler <jasa.knap@gmail.com>
2022-08-27 11:05:31 +0200

    Formatirana spletni vmesnik in model

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 STANJE = "stanje.json"
 
+
 @dataclass
 class Uporabnik:
     uporabnisko_ime: str
@@ -29,7 +30,6 @@ class Uporabnik:
             zasifrirano_geslo=slovar["zasifrirano_geslo"],
             igre=slovar["igre"]
         )
-
 
 
 @dataclass
@@ -78,7 +78,7 @@ class VseSkupaj:
             lokalni_rezultat_nasprotnik = "Remi"
 
         return rezultat_igre, lokalni_rezultat, lokalni_rezultat_nasprotnik
-    
+
     @staticmethod
     def vnesi_novega_uporabnika(uporabnisko_ime, geslo_v_cistopisu, vse_skupaj):
         slovar_z_novim_uporabnikom = dict()
@@ -88,10 +88,10 @@ class VseSkupaj:
             "uporabniki"] + [nov_uporabnik]
         VseSkupaj.v_datoteko(slovar_z_novim_uporabnikom, STANJE)
         vse_skupaj = VseSkupaj.iz_datoteke(STANJE)
-        
 
         return vse_skupaj
-    
+
+
 @dataclass
 class PrikazovanjeStrani:
     """Razred, ki poskrbi, da je v spletnem vmesniku malo kode
@@ -100,7 +100,7 @@ class PrikazovanjeStrani:
         Funkcije vrnejo potrebne spemenljivke ustreznih tipov
     """
     @staticmethod
-    def arhiv_igra(id, vse_uporabnikove_igre):        
+    def arhiv_igra(id, vse_uporabnikove_igre):
         for posamezna_igra in vse_uporabnikove_igre:
             if str(posamezna_igra["id"]) == id[:-1]:
                 igra = posamezna_igra["igra"]
@@ -109,34 +109,33 @@ class PrikazovanjeStrani:
                 celoten_fen = posamezna_igra["celoten_fen"].split(",")
                 popravljen_celoten_fen = [i.split(" ")[0] for i in celoten_fen]
                 return igra, beli, crni, popravljen_celoten_fen
-            
-    
-    @staticmethod
-    def statistika(uporabnisko_ime, uporabnik):        
-        vse_uporabnikove_igre = uporabnik.igre
-        
-        mozni_razpleti = ["zmage", "porazi", "remiji", "zmage_beli", "porazi_beli", "remiji_beli", "zmage_crni", "porazi_crni", "remiji_crni"]
-        slovar_rezultatov = {razplet:0 for razplet in mozni_razpleti}
 
-        
+    @staticmethod
+    def statistika(uporabnisko_ime, uporabnik):
+        vse_uporabnikove_igre = uporabnik.igre
+
+        mozni_razpleti = ["zmage", "porazi", "remiji", "zmage_beli",
+                          "porazi_beli", "remiji_beli", "zmage_crni", "porazi_crni", "remiji_crni"]
+        slovar_rezultatov = {razplet: 0 for razplet in mozni_razpleti}
+
         def pridobi_podatek_o_odstotkih(razplet):
             if "beli" in razplet:
                 if slovar_rezultatov["zmage_beli"] + slovar_rezultatov["porazi_beli"] + slovar_rezultatov["remiji_beli"] != 0:
                     return '{:.1%}'.format(slovar_rezultatov[razplet]/(slovar_rezultatov["zmage_beli"] + slovar_rezultatov["porazi_beli"] + slovar_rezultatov["remiji_beli"]))
                 else:
                     return "ni podatka"
-            
+
             if "crni" in razplet:
                 if slovar_rezultatov["zmage_crni"] + slovar_rezultatov["porazi_crni"] + slovar_rezultatov["remiji_crni"] != 0:
                     return '{:.1%}'.format(slovar_rezultatov[razplet]/(slovar_rezultatov["zmage_crni"] + slovar_rezultatov["porazi_crni"] + slovar_rezultatov["remiji_crni"]))
                 else:
                     return "ni podatka"
-            
+
             if slovar_rezultatov["zmage"] + slovar_rezultatov["porazi"] + slovar_rezultatov["remiji"] != 0:
-                    return '{:.1%}'.format(slovar_rezultatov[razplet]/(slovar_rezultatov["zmage"] + slovar_rezultatov["porazi"] + slovar_rezultatov["remiji"]))
+                return '{:.1%}'.format(slovar_rezultatov[razplet]/(slovar_rezultatov["zmage"] + slovar_rezultatov["porazi"] + slovar_rezultatov["remiji"]))
             else:
                 return "ni podatka"
-           
+
         for posamezna_igra in vse_uporabnikove_igre:
             uporabnik_je_bel = posamezna_igra["beli"] == uporabnisko_ime
             if posamezna_igra["lokalni_rezultat"] == "Zmaga":
@@ -157,13 +156,12 @@ class PrikazovanjeStrani:
                     slovar_rezultatov["remiji_beli"] += 1
                 else:
                     slovar_rezultatov["remiji_crni"] += 1
-                           
-        slovar_rezultatov_s_podatki = {i: pridobi_podatek_o_odstotkih(i) for i in slovar_rezultatov}
-        
-        return slovar_rezultatov, slovar_rezultatov_s_podatki
-        
 
-    
+        slovar_rezultatov_s_podatki = {
+            i: pridobi_podatek_o_odstotkih(i) for i in slovar_rezultatov}
+
+        return slovar_rezultatov, slovar_rezultatov_s_podatki
+
     @staticmethod
     def shrani_igro(vse_skupaj, igra, uporabnisko_ime, beli, crni, celoten_fen):
         if uporabnisko_ime == beli:
@@ -173,49 +171,47 @@ class PrikazovanjeStrani:
 
         rezultat_igre, lokalni_rezultat, lokalni_rezultat_nasprotnik = VseSkupaj.doloci_lastnosti_odigrane_igre(
             igra, uporabnisko_ime, beli, crni)
-        
+
         seznam_ki_mu_hocemo_dodati_igro = vse_skupaj.v_slovar()["uporabniki"]
         nov_seznam = []
         for account in seznam_ki_mu_hocemo_dodati_igro:
             if account["uporabnisko_ime"] == uporabnisko_ime:
                 account["igre"] = account["igre"] + [{"id": len(account["igre"]) + 1, "beli":beli, "crni":crni, "rezultat": rezultat_igre,
-                                                    "lokalni_rezultat": lokalni_rezultat, "igra":igra, "celoten_fen": celoten_fen, "datum":str(datetime.today())}]
+                                                      "lokalni_rezultat": lokalni_rezultat, "igra":igra, "celoten_fen": celoten_fen, "datum":str(datetime.today())}]
             if account["uporabnisko_ime"] == nasprotnik:
                 account["igre"] = account["igre"] + [{"id": len(account["igre"]) + 1, "beli":beli, "crni":crni, "rezultat": rezultat_igre,
-                                                    "lokalni_rezultat": lokalni_rezultat_nasprotnik, "igra":igra, "celoten_fen": celoten_fen, "datum":str(datetime.today())}]
+                                                      "lokalni_rezultat": lokalni_rezultat_nasprotnik, "igra":igra, "celoten_fen": celoten_fen, "datum":str(datetime.today())}]
             nov_seznam.append(account)
         VseSkupaj.v_datoteko({"uporabniki": nov_seznam}, STANJE)
         vse_skupaj = VseSkupaj.iz_datoteke(STANJE)
         return vse_skupaj
-        
-        
+
     @staticmethod
     def igraj_proti_cloveku_doloci_napako(beli, crni):
         napaka = ""
         if beli in ["Stanley", "Stockfish", "Stocknoob"] or crni in ["Stanley", "Stockfish", "Stocknoob"]:
-            napaka="To ime je rezervirano za računalnike!"
+            napaka = "To ime je rezervirano za računalnike!"
         if len(beli) > 20 or len(crni) > 20:
-            napaka="Ime nasprotnika sme vsebovati največ 20 znakov!"
+            napaka = "Ime nasprotnika sme vsebovati največ 20 znakov!"
         if beli == "" or crni == "":
             napaka = "Nasprotnik mora imeti ime!"
         if not beli.isascii() or not crni.isascii():
-            napaka="Nasprotnik mora imeti ASCII sprejemljivo ime!"
+            napaka = "Nasprotnik mora imeti ASCII sprejemljivo ime!"
         if beli == crni:
-            napaka="Imeni igralcev morata biti različni!"
+            napaka = "Imeni igralcev morata biti različni!"
         return napaka
-    
-            
+
     @staticmethod
     def registracija_doloci_napako(uporabnisko_ime):
         # Pred registracijo preprečimo nekaj problematičnih primerov
         napaka = ""
         if uporabnisko_ime in ["Stanley", "Stockfish", "Stocknoob"]:
-            napaka="To ime je rezervirano za računalnike!"
+            napaka = "To ime je rezervirano za računalnike!"
         if not uporabnisko_ime.isascii():
-            napaka="Ime uporabnika mora biti ASCII sprejemljivo!"
+            napaka = "Ime uporabnika mora biti ASCII sprejemljivo!"
         if len(uporabnisko_ime) > 20:
-            napaka="Ime uporabnika sme vsebovati največ 20 znakov!"
+            napaka = "Ime uporabnika sme vsebovati največ 20 znakov!"
         if len(uporabnisko_ime) == 0:
-            napaka="Ime uporabnika ne sme biti prazno!"
-        
+            napaka = "Ime uporabnika ne sme biti prazno!"
+
         return napaka
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -6,14 +6,17 @@ SKRIVNOST = model.VseSkupaj.preberi_skrivnost_iz_datoteke()
 STANJE = model.STANJE
 vse_skupaj = model.VseSkupaj.iz_datoteke(STANJE)
 
-def poisci_uporabnika(uporabnisko_ime = None, prijavljanje = False):
+
+def poisci_uporabnika(uporabnisko_ime=None, prijavljanje=False):
     if prijavljanje == False:
-        uporabnisko_ime = bottle.request.get_cookie('uporabnisko_ime', secret=SKRIVNOST)
+        uporabnisko_ime = bottle.request.get_cookie(
+            'uporabnisko_ime', secret=SKRIVNOST)
     if uporabnisko_ime is None and not prijavljanje:
         bottle.redirect('/')
     for uporabnik in vse_skupaj.uporabniki:
         if uporabnik.uporabnisko_ime == uporabnisko_ime:
             return uporabnik
+
 
 @bottle.get('/')
 def index():
@@ -36,7 +39,7 @@ def prijava_post():
     uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
     geslo_v_cistopisu = bottle.request.forms.getunicode("zasifrirano_geslo")
     uporabnik = poisci_uporabnika(
-        uporabnisko_ime, prijavljanje= True)
+        uporabnisko_ime, prijavljanje=True)
     if uporabnik:
         if uporabnik.zasifrirano_geslo == model.Uporabnik.zasifriraj_geslo(geslo_v_cistopisu):
             bottle.response.set_cookie(
@@ -58,18 +61,20 @@ def prijava_post():
     global vse_skupaj
     uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
     geslo_v_cistopisu = bottle.request.forms.getunicode("zasifrirano_geslo")
-    
+
     uporabnik = poisci_uporabnika(
-            uporabnisko_ime, prijavljanje= True)
+        uporabnisko_ime, prijavljanje=True)
     if uporabnik:
         napaka = "Uporabnik že obstaja!"
     else:
-        napaka = model.PrikazovanjeStrani.registracija_doloci_napako(uporabnisko_ime)
-    
+        napaka = model.PrikazovanjeStrani.registracija_doloci_napako(
+            uporabnisko_ime)
+
     if napaka:
         return bottle.template("registracija.tpl", napaka=napaka)
     else:
-        vse_skupaj = model.VseSkupaj.vnesi_novega_uporabnika(uporabnisko_ime, geslo_v_cistopisu, vse_skupaj)
+        vse_skupaj = model.VseSkupaj.vnesi_novega_uporabnika(
+            uporabnisko_ime, geslo_v_cistopisu, vse_skupaj)
         bottle.response.set_cookie(
             "uporabnisko_ime", uporabnisko_ime, path="/", secret=SKRIVNOST)
         bottle.redirect("/")
@@ -81,12 +86,14 @@ def igraj_proti_racunalniku_get():
     return bottle.template("igraj.tpl", vrsta_igre="racunalnik")
 
 # Tole preveri oba primera: Stanley in Stockfish (po novem tudi Stocknoob)
+
+
 @bottle.get("/igraj_proti_racunalniku/<racunalniski_nasprotnik:path>/")
 def server_static(racunalniski_nasprotnik):
     poisci_uporabnika()
-    uporabnisko_ime = bottle.request.get_cookie('uporabnisko_ime', secret=SKRIVNOST)
-    return bottle.template(f"igraj_{racunalniski_nasprotnik}.tpl", SKRIVNOST = SKRIVNOST, uporabnisko_ime = uporabnisko_ime)
-
+    uporabnisko_ime = bottle.request.get_cookie(
+        'uporabnisko_ime', secret=SKRIVNOST)
+    return bottle.template(f"igraj_{racunalniski_nasprotnik}.tpl", SKRIVNOST=SKRIVNOST, uporabnisko_ime=uporabnisko_ime)
 
 
 # To preveri oba primera: Stanley in Stockfish
@@ -107,28 +114,28 @@ def server_static(racunalniski_nasprotnik, barva):
 def igra_proti_cloveku_get():
     uporabnisko_ime = poisci_uporabnika().uporabnisko_ime
     uporabniki = vse_skupaj.uporabniki
-    return bottle.template("igraj.tpl", vrsta_igre="clovek", uporabnisko_ime = uporabnisko_ime, uporabniki = uporabniki, nasprotnik=None, napaka=None)
+    return bottle.template("igraj.tpl", vrsta_igre="clovek", uporabnisko_ime=uporabnisko_ime, uporabniki=uporabniki, nasprotnik=None, napaka=None)
 
 
 @bottle.post("/igraj_proti_cloveku/")
 def igraj_proti_racunalniku_post():
-    
+
     uporabnisko_ime = poisci_uporabnika().uporabnisko_ime
     uporabniki = vse_skupaj.uporabniki
     beli = bottle.request.forms.getunicode("beli")
     crni = bottle.request.forms.getunicode("crni")
-    napaka = model.PrikazovanjeStrani.igraj_proti_cloveku_doloci_napako(beli = beli, crni = crni)
+    napaka = model.PrikazovanjeStrani.igraj_proti_cloveku_doloci_napako(
+        beli=beli, crni=crni)
     if napaka:
         return bottle.template("igraj.tpl", vrsta_igre="clovek",
-                               uporabnisko_ime = uporabnisko_ime, uporabniki = uporabniki, nasprotnik=None, beli=beli, crni=crni, napaka=napaka)
+                               uporabnisko_ime=uporabnisko_ime, uporabniki=uporabniki, nasprotnik=None, beli=beli, crni=crni, napaka=napaka)
     else:
         bottle.response.set_cookie(
             "beli", beli, path="/shrani_igro/", secret=SKRIVNOST)
         bottle.response.set_cookie(
             "crni", crni, path="/shrani_igro/", secret=SKRIVNOST)
         return bottle.template("igraj.tpl", vrsta_igre="clovek",
-                               uporabnisko_ime = uporabnisko_ime, uporabniki = uporabniki, nasprotnik="izbran", beli=beli, crni=crni, napaka=None)
-
+                               uporabnisko_ime=uporabnisko_ime, uporabniki=uporabniki, nasprotnik="izbran", beli=beli, crni=crni, napaka=None)
 
 
 @bottle.route("/shrani_igro/")
@@ -140,7 +147,8 @@ def shrani_igro():
     celoten_fen = bottle.request.query.fen.replace("_", "/")
     beli = bottle.request.get_cookie("beli", secret=SKRIVNOST)
     crni = bottle.request.get_cookie("crni", secret=SKRIVNOST)
-    vse_skupaj = model.PrikazovanjeStrani.shrani_igro(vse_skupaj, igra=igra, uporabnisko_ime=uporabnisko_ime, beli=beli, crni=crni, celoten_fen=celoten_fen)
+    vse_skupaj = model.PrikazovanjeStrani.shrani_igro(
+        vse_skupaj, igra=igra, uporabnisko_ime=uporabnisko_ime, beli=beli, crni=crni, celoten_fen=celoten_fen)
     bottle.redirect("/")
 
 
@@ -148,8 +156,9 @@ def shrani_igro():
 def arhiv_get():
     uporabnisko_ime = poisci_uporabnika().uporabnisko_ime
     uporabnik = poisci_uporabnika(uporabnisko_ime)
-    slovar_rezultatov, slovar_rezultatov_s_podatki = model.PrikazovanjeStrani.statistika(uporabnisko_ime, uporabnik)
-    return bottle.template("statistika.tpl", uporabnisko_ime = uporabnisko_ime, slovar_rezultatov = slovar_rezultatov, slovar_rezultatov_s_podatki = slovar_rezultatov_s_podatki)
+    slovar_rezultatov, slovar_rezultatov_s_podatki = model.PrikazovanjeStrani.statistika(
+        uporabnisko_ime, uporabnik)
+    return bottle.template("statistika.tpl", uporabnisko_ime=uporabnisko_ime, slovar_rezultatov=slovar_rezultatov, slovar_rezultatov_s_podatki=slovar_rezultatov_s_podatki)
 
 
 @bottle.get("/arhiv/")
@@ -157,7 +166,7 @@ def arhiv_get():
     uporabnisko_ime = poisci_uporabnika().uporabnisko_ime
     uporabnik = poisci_uporabnika(uporabnisko_ime)
     vse_uporabnikove_igre = uporabnik.igre
-    return bottle.template("arhiv.tpl", uporabnisko_ime = uporabnisko_ime, vse_uporabnikove_igre = vse_uporabnikove_igre)
+    return bottle.template("arhiv.tpl", uporabnisko_ime=uporabnisko_ime, vse_uporabnikove_igre=vse_uporabnikove_igre)
 
 
 @bottle.get("/arhiv/<id:path>")
@@ -165,8 +174,9 @@ def server_static(id):
     uporabnisko_ime = poisci_uporabnika().uporabnisko_ime
     uporabnik = poisci_uporabnika(uporabnisko_ime)
     vse_uporabnikove_igre = uporabnik.igre
-    igra, beli, crni, popravljen_celoten_fen = model.PrikazovanjeStrani.arhiv_igra(id, vse_uporabnikove_igre = vse_uporabnikove_igre)
-    return bottle.template("arhiv_igra.tpl", SKRIVNOST=SKRIVNOST, STANJE=STANJE, vse_skupaj=vse_skupaj, uporabnisko_ime = uporabnisko_ime, igra = igra, beli = beli, crni = crni, popravljen_celoten_fen = popravljen_celoten_fen, id=id[:-1])
+    igra, beli, crni, popravljen_celoten_fen = model.PrikazovanjeStrani.arhiv_igra(
+        id, vse_uporabnikove_igre=vse_uporabnikove_igre)
+    return bottle.template("arhiv_igra.tpl", SKRIVNOST=SKRIVNOST, STANJE=STANJE, vse_skupaj=vse_skupaj, uporabnisko_ime=uporabnisko_ime, igra=igra, beli=beli, crni=crni, popravljen_celoten_fen=popravljen_celoten_fen, id=id[:-1])
 
 
 # Tole pobriše piškotek uporabnisko_ime ob odjavi
